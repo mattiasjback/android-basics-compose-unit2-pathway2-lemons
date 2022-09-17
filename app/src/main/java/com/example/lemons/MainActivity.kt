36d7af8e4c06b916bc79dd628a058632dd9af10a
Mattias Bäck <mattias.back@bontouch.com>
2022-09-17 09:05:16 +0200

    Add logic for state tranistions between lemon app states

--- app/src/main/java/com/example/lemons/MainActivity.kt
+++ app/src/main/java/com/example/lemons/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -31,24 +32,94 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
+fun DefaultPreview() {
+    LemonsTheme {
+        LemonsApp()
+    }
+}
+
+private val lemonResources =  mapOf(
+    1 to Pair(R.drawable.lemon_tree, R.string.tap_lemon_tree),
+    2 to Pair(R.drawable.lemon_squeeze, R.string.keep_tapping_lemon),
+    3 to Pair(R.drawable.lemon_drink, R.string.tap_lemonade),
+    4 to Pair(R.drawable.lemon_restart, R.string.tap_empty_glass)
+)
+
+@Composable
 fun LemonsApp() {
+
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    val resources = lemonResources[currentStep]!!
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        when(currentStep) {
+            1 -> LemonTextAndImage(
+                imageId = resources.first,
+                textId = resources.second,
+                onImageClick = {
+                    currentStep++
+                    squeezeCount = (1..4).random()
+                }
+            )
+            2 -> LemonTextAndImage(
+                imageId = resources.first,
+                textId = resources.second,
+                onImageClick = {
+                    squeezeCount--
+                    if(squeezeCount == 0) {
+                        currentStep++
+                    }
+                }
+            )
+            3 -> LemonTextAndImage(
+                imageId = resources.first,
+                textId = resources.second,
+                onImageClick = {
+                    currentStep++
+                }
+            )
+            4 -> LemonTextAndImage(
+                imageId = resources.first,
+                textId = resources.second,
+                onImageClick = {
+                    currentStep = 1
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    imageId: Int,
+    textId: Int,
+    onImageClick: () -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = stringResource(id = R.string.tap_lemon_tree),
+            text = stringResource(id = textId),
             fontSize = 18.sp
         )
         Spacer(modifier = Modifier
             .fillMaxWidth()
             .height(16.dp)
         )
-        val image = painterResource(id = R.drawable.lemon_tree)
+        val image = painterResource(id = imageId)
         Image(
             painter = image,
-            contentDescription = stringResource(id = R.string.desc_lemon_tree)
+            contentDescription = "click the lemon",
+            modifier = Modifier.clickable {
+                onImageClick()
+            }
         )
     }
 }
